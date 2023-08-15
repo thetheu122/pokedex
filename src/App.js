53379a21ff109fbe559a9d30941c4338d7d344ea
thetheu122 <math.santosmed@gmail.com>
2023-08-15 11:08:45 -0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import axios from 'axios';
 
-function App() {
+export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  const [offset, setOffset] = useState(0)
+  const [mensagem, setMensagem] = useState('Encontrar Pokémons')
+  const [verify, setVerify] = useState(false)
+
+
+  async function buscarPokemons() {
+    
+    try {
+      let url = 'https://pokeapi.co/api/v2/pokemon?limit=8&offset=' + offset;
+      let response = await axios.get(url);
+      let listaPokemons = [];
+
+      for (let item of response.data.results) {
+        let pokemonResp = await axios.get(item.url);
+        let imagem = pokemonResp.data.sprites.other['official-artwork'].front_default;
+
+          let tipos = pokemonResp.data.types.map((t) => t.type.name).join(', ');
+
+          listaPokemons.push({
+          nome: maiusculo(item.name),
+          imagem: imagem,
+          tipos: tipos,
+        });
+      }
+
+      setPokemons(listaPokemons);
+    } catch (error) {
+      console.error('Erro ao buscar pokémons:', error);
+    }
+    setOffset(offset + 8)
+
+    if(verify == false){
+      setMensagem('Encontrar mais Pokémons')
+    }
+  }
+
+  function maiusculo(p) {
+    return p.charAt(0).toUpperCase() + p.slice(1);
+  }
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Pokedex">
+      <div className='topo-pokemon'>
+        <img src='./assets/image/logoP.png' alt="Pokemon Logo" />
+        <button onClick={buscarPokemons}>{mensagem}</button>
+      </div>
+
+      <div className='s2-cards'>
+        {pokemons.map((item, index) => (
+          <div className='cards' key={index}>
+            <div className='card-foto'>
+              <img className='foto-pok' src={item.imagem} alt={item.nome} />
+            </div>
+            <h3>{item.nome}</h3>
+            <p>{item.tipos}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
